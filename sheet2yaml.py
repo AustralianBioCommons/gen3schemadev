7b150110f2d2184a0253489b7fadf4ae73b348fc
Marion <mshadbolt@users.noreply.github.com>
2021-12-07 16:02:14 +1100

    remove unneeded dbgap props

--- sheet2yaml.py
+++ sheet2yaml.py
@@ -6,8 +6,19 @@ import networkx as nx
 def main():
     pass
 
+
 if __name__ == "__main__":
     sheet_id = "1AX9HLzIV6wtkVylLkwOr3kdKDaZf4ukeYACTJ7lYngk"
+
+    objects_gid = "1815863480"
+    url = "https://docs.google.com/spreadsheets/d/{}/export?format=csv&gid={}".format(sheet_id, objects_gid)
+    objects = pd.read_csv(url)
+
+    links_gid = "1073266181"
+    url = "https://docs.google.com/spreadsheets/d/{}/export?format=csv&gid={}".format(sheet_id, links_gid)
+    links = pd.read_csv(url)
+    links.replace({np.nan: None}, inplace=True)
+
     properties_gid = "804936807"
     url = "https://docs.google.com/spreadsheets/d/{}/export?format=csv&gid={}".format(sheet_id, properties_gid)
     properties = pd.read_csv(url)
@@ -20,9 +31,42 @@ if __name__ == "__main__":
 
     bundle = gen3schemadev.ConfigBundle("schema/cad")
 
-    for object_name in properties.OBJECT.unique():
-        g3_obj = bundle.objects["%s.yaml"%object_name]
-        object_fields = properties.loc[properties.OBJECT == object_name]
+    for idx, row in objects.iterrows():
+        # parse object definition
+        g3_obj = bundle.objects["%s.yaml"%row.ID]
+        g3_obj.set_object_definitions(row.ID, row.TITLE, row.CATEGORY, row.DESCRIPTION, row.NAMESPACE)
+
+        # parse link definitions
+        link_rows = links[links['SCHEMA'] == row.ID]
+        if len(link_rows) > 0:
+            links_list = []
+            single_links = link_rows[link_rows.SUBGROUP.values == None]
+            group_links = link_rows[link_rows.SUBGROUP.values != None]
+            for each_idx, each_row in single_links.iterrows():
+                this_link = gen3schemadev.Gen3Link(each_row.NAME, each_row.BACKREF, each_row.LABEL, each_row.SCHEMA,
+                                                   gen3schemadev.Gen3Link.MULTIPLICITY(each_row.MULTIPLICITY),
+                                                   each_row.REQUIRED)
+                links_list.append(this_link)
+            if len(group_links) > 0:
+                subgroups = list(set(group_links.SUBGROUP.tolist()))
+                for subgroup in subgroups:
+                    this_subgroup = group_links[group_links['SUBGROUP'] == subgroup]
+                    subgroup_dict = {"exclusive": group_links.EXCLUSIVE[0],
+                                     "required": group_links.SG_REQUIRED[0],
+                                     "subgroup": []}
+                    for this_idx, each_row in this_subgroup.iterrows():
+                        this_link = {"name": each_row.NAME,
+                                     "backref": each_row.BACKREF,
+                                     "label": each_row.LABEL,
+                                     "target_type": each_row.SCHEMA,
+                                     "multiplicity": each_row.MULTIPLICITY,
+                                     "required": each_row.REQUIRED}
+                        subgroup_dict['subgroup'].append(this_link)
+                    links_list.append(gen3schemadev.Gen3LinkGroup.from_dict(subgroup_dict))
+            g3_obj.set_links(links_list)
+
+        # parse property definitions
+        object_fields = properties.loc[properties.OBJECT == row.ID]
         for idx,field in object_fields.iterrows():
             if field.TYPE == "datetime":
                 g3_obj.add_property(gen3schemadev.Gen3DatetimeProperty(field.VARIABLE_NAME,field.DESCRIPTION))
@@ -46,9 +90,5 @@ if __name__ == "__main__":
 
             if field.REQUIRED:
                 g3_obj.add_required(field.VARIABLE_NAME)
-
-
-    for object in bundle.objects:
-        bundle.objects[object].namespace = "https://gen3.biocommons.org.au"
 
     bundle.dump("schema_out/")
