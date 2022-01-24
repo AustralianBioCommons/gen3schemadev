67ac6cf6710830f6d0e10ccb199c2faec063e460
Uwe Winter <uwwint@gmail.com>
2022-01-24 14:45:24 +1100

    refactored the lib for merging.

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
    author='Uwe Winter',
    author_email='uwe@biocommons.org.au',
    packages=['gen3schemadev'],  #same as name
-   install_requires=['PyYAML~=6.0'], #external packages as dependencies
+   install_requires=["setuptools~=58.5.3","PyYAML~=6.0","pandas~=1.3.5","openpyxl","oyaml~=1.0","networkx~=2.6.3","matplotlib","ete3","dictionaryutils~=3.0.0","numpy~=1.22.1"], #external packages as dependencies
 )
