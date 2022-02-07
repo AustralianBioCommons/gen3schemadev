568d369c7981eb8d677e53e71c11208a5d718fc9
Uwe Winter <uwwint@gmail.com>
2022-02-07 15:30:53 +1100

    added 1sec wait between api calls.

--- scripts/stop_simulator_instance.sh
+++ scripts/stop_simulator_instance.sh
@@ -1,4 +1,5 @@
 aws ec2 stop-instances --instance-ids i-001632cdd600d853d
 while [ "$(aws ec2 describe-instances --instance-ids i-001632cdd600d853d | jq -r '.Reservations[].Instances[].PublicIpAddress')" != "null" ]; do 
 	echo "Waiting for shutdown"
+	sleep 1
 done
