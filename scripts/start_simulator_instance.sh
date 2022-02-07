a16071650ee21b90d6018f66288f56b8f4f300a7
Uwe Winter <uwwint@gmail.com>
2022-02-07 15:17:46 +1100

    added instance shutdown and start

---
+++ scripts/start_simulator_instance.sh
@@ -0,0 +1,6 @@
+aws ec2 start-instances --instance-ids i-001632cdd600d853d
+while [ "$(aws ec2 describe-instances --instance-ids i-001632cdd600d853d | jq -r '.Reservations[].Instances[].PublicIpAddress')" = "null" ]; do 
+	echo "Waiting for machine to start"
+done
+
+aws ec2 describe-instances --instance-ids i-001632cdd600d853d | jq -r ".Reservations[].Instances[].PublicIpAddress"
