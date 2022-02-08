bab5d3b07646ea940c0556aeac154524ff74456f
Uwe Winter <uwwint@gmail.com>
2022-02-08 13:22:03 +1100

    updated start and stopping scripts

--- scripts/start_simulator_instance.sh
+++ scripts/start_simulator_instance.sh
@@ -1,5 +1,5 @@
 aws ec2 start-instances --instance-ids i-001632cdd600d853d
-while [ "$(aws ec2 describe-instances --instance-ids i-001632cdd600d853d | jq -r '.Reservations[].Instances[].PublicIpAddress')" = "null" ]; do 
+while [ "$(aws ec2 describe-instances --instance-ids i-001632cdd600d853d | jq -r '.Reservations[].Instances[].State.Name')" != "running" ]; do 
 	echo "Waiting for machine to start"
 	sleep 1
 done
--- scripts/stop_simulator_instance.sh
+++ scripts/stop_simulator_instance.sh
@@ -1,5 +1,5 @@
 aws ec2 stop-instances --instance-ids i-001632cdd600d853d
-while [ "$(aws ec2 describe-instances --instance-ids i-001632cdd600d853d | jq -r '.Reservations[].Instances[].PublicIpAddress')" != "null" ]; do 
+while [ "$(aws ec2 describe-instances --instance-ids i-001632cdd600d853d | jq -r '.Reservations[].Instances[].State.Name')" != "stopped" ]; do 
 	echo "Waiting for shutdown"
 	sleep 1
 done
