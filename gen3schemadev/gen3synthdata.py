a9ebfa1b1475f07969b1d6f9dc08c16865c8730f
JoshuaHarris391 <harjo391@gmail.com>
2024-08-16 15:43:04 +1000

    added methods to just add a dummy file name rather than copying individual dummy files

--- gen3schemadev/gen3synthdata.py
+++ gen3schemadev/gen3synthdata.py
@@ -3,6 +3,7 @@ import shutil
 import json
 import yaml
 import uuid
+import glob
 
 
 class gen3SynthFiles:
@@ -11,6 +12,8 @@ class gen3SynthFiles:
         self.project_id_list = project_id_list
         self.templates_path = templates_path
         self.output_dir = output_dir
+        self.all_metadata_paths = self.list_all_metadata_paths()
+        self.metadata_file_list = self.list_metadata_files()
         self.data_list = []
         self.synthetic_file_index = {}
 
@@ -29,6 +32,83 @@ class gen3SynthFiles:
                 with open(os.path.join(self.json_data_path, project_id, filename), 'r') as f:
                     self.data_list.extend(json.load(f))
         print("Completed parsing JSON data.")  # Debugging print
+        
+    def read_json_file(self, file_path):
+        """
+        Reads a JSON file from the given file path and returns the data.
+        
+        :param file_path: Path to the JSON file.
+        :return: Data contained in the JSON file.
+        """
+        print(f"Reading JSON file from: {file_path}")  # Debugging print
+        try:
+            with open(file_path, 'r') as f:
+                data = json.load(f)
+            print("Completed reading JSON file.")  # Debugging print
+            return data
+        except FileNotFoundError:
+            print(f"Error: The file {file_path} does not exist.")
+        except json.JSONDecodeError as exc:
+            print(f"Error parsing JSON file: {exc}")
+        return None
+    
+    def write_json_file(self, data, file_path):
+        """
+        Writes the given data to a JSON file at the specified file path.
+
+        :param data: Data to be written to the JSON file.
+        :param file_path: Path where the JSON file will be written.
+        """
+        print(f"Writing JSON file to: {file_path}")  # Debugging print
+        try:
+            # Extract directory path from file_path
+            dir_path = os.path.dirname(file_path)
+            # Create directory if it does not exist
+            os.makedirs(dir_path, exist_ok=True)
+            
+            with open(file_path, 'w') as f:
+                json.dump(data, f, indent=4)
+            print("Completed writing JSON file.")  # Debugging print
+        except IOError as exc:
+            print(f"Error writing JSON file: {exc}")
+            
+    def list_all_metadata_paths(self, print_list: bool = False):
+        all_outputs = []
+
+        for project_id in self.project_id_list:
+            project_path = os.path.join(self.json_data_path, project_id)
+            filenames = os.listdir(project_path)
+
+            for filename in filenames:
+                output = {project_id: filename}
+                all_outputs.append(output)
+
+        if print_list:
+            for entry in self.metadata_file_list:
+                print(entry)
+                
+        return all_outputs
+        
+    def list_metadata_files(self, print_list: bool = False):
+        all_outputs = []
+
+        for project_id in self.project_id_list:
+            project_path = os.path.join(self.json_data_path, project_id)
+            filenames = os.listdir(project_path)
+            filtered_filenames = [
+                filename for filename in filenames if filename.endswith('file.json')
+            ]
+
+            for filename in filtered_filenames:
+                output = {project_id: filename}
+                all_outputs.append(output)
+
+        if print_list:
+            for entry in self.metadata_file_list:
+                print(entry)
+                
+        return all_outputs
+        
 
     def read_synthetic_file_index(self):
         index_file_path = os.path.join(self.templates_path, 'synthetic_file_index.yaml')
@@ -47,7 +127,7 @@ class gen3SynthFiles:
         matched_fn = None
         for synth_fn, synth_lookup in self.synthetic_file_index.items():
             if any(data_format in x for x in synth_lookup):
-                print(f'data format: {data_format} found in: {synth_fn}')
+                # print(f'data format: {data_format} found in: {synth_fn}')
                 return synth_fn
         if matched_fn is None:
             print(f'WARNING: "{data_format}" does not match any synthetic files')
@@ -67,16 +147,57 @@ class gen3SynthFiles:
         shutil.copy(src, dst)
         print("Completed copying file.")  # Debugging print
 
-    def synth_data_file_gen(self):
+    def copy_dummy_files(self, preserve_dummy_names=False):
         for project_id in self.project_id_list:
             self.data_list = []
             self.parse_json_data(project_id)
             print(f"Total number of data objects: {len(self.data_list)}")
+            
             for data_object in self.data_list:
                 print(f"\nProcessing data object: {data_object}")  # Debugging print
-                file_name = data_object['file_name']
                 file_format = data_object['data_format']
                 synth_file = self.find_dummy_file(file_format)
+                
+                if preserve_dummy_names:
+                    file_name = synth_file
+                else:
+                    file_name = data_object['file_name']
+                
                 final_file_name = f'{file_name}.{file_format}'
                 self.copy_file(synth_file, final_file_name, project_id)
                 print(f"SUCCESS: Completed processing for file: {final_file_name}")  # Debugging print
+                
+                
+    def copy_metadata_files(self):
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
+        
+        for entry in self.all_metadata_paths:
+            for project, filename in entry.items():
+                print(f"Processing {project}: {filename}")  # Debugging print
+                source_path = os.path.join(self.json_data_path, project, filename)
+                dest_path = os.path.join(self.output_dir, project, filename)
+                if not os.path.exists(source_path):
+                    print(f"Error: The source file {source_path} does not exist.")
+                    continue
+                if not os.path.exists(os.path.dirname(dest_path)):
+                    os.makedirs(os.path.dirname(dest_path))
+                shutil.copy(source_path, dest_path)
+                print(f"Copied {source_path} to {dest_path}")  # Debugging print
+            
+
+            
+                
+    def update_fn_metadata(self):
+        # reading a metadata file
+        for entry in self.metadata_file_list:
+            for project, filename in entry.items():
+                print(f"Processing {project}: {filename}")  # Debugging print
+                metadata = self.read_json_file(os.path.join(self.json_data_path, project, filename))
+                
+                for data_object in metadata:
+                    file_format = data_object['data_format']
+                    synth_filename = self.find_dummy_file(file_format)
+                    data_object['file_name'] = synth_filename
+                
+                self.write_json_file(metadata, os.path.join(self.output_dir, project, filename))
